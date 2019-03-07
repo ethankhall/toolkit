08665fc13fc4da93be8c839dbceaf59b3971eb0d
Ethan Hall <ethankhall@pm.me>
2019-03-06 16:39:34 -0800

    Allow NSQ/POST to support older NSQ API
    Also fix publishing to use the right names of artifacts

--- src/commands/nsq/api.rs
+++ src/commands/nsq/api.rs
@@ -1,6 +1,11 @@
 use url::Url;
 
 #[derive(Serialize, Deserialize)]
+pub struct StatusTopicsResponse {
+    pub data: StatusTopicsDetails
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct StatusTopicsDetails {
     pub topics: Vec<TopicDetails>
 }
@@ -90,7 +95,12 @@ pub fn get_topic_status(base_url: &str, topic: &str) -> Option<StatusTopicsDetai
             if let Ok(root) = json_body {
                 return Some(root);
             } else {
-                warn!("Unable to deserialize {} from the stats", body);
+                let json_body: Result<StatusTopicsResponse, _> = serde_json::from_str(&body);
+                if let Ok(root) = json_body {
+                    return Some(root.data);
+                } else {
+                    warn!("Unable to deserialize {} from the stats", body);
+                }
             }
         }
     }
@@ -195,6 +205,90 @@ fn test_extract_size() {
     let body: StatusTopicsDetails = serde_json::from_str(body).unwrap();
 
     let (queued, in_flight) = extract_size_from_body(body, "foo").unwrap();
+    assert_eq!(3, queued);
+    assert_eq!(1, in_flight);
+}
+
+#[test]
+fn older_api_test() {
+    let body = "{
+  \"status_code\": 200,
+  \"status_txt\": \"OK\",
+  \"data\": {
+    \"version\": \"0.3.8\",
+    \"health\": \"OK\",
+    \"start_time\": 1543350728,
+    \"topics\": [
+        {
+            \"topic_name\": \"foo\",
+            \"channels\": [
+                {
+                    \"channel_name\": \"tail180292#ephemeral\",
+                    \"depth\": 3,
+                    \"backend_depth\": 0,
+                    \"in_flight_count\": 1,
+                    \"deferred_count\": 0,
+                    \"message_count\": 1399,
+                    \"requeue_count\": 0,
+                    \"timeout_count\": 0,
+                    \"clients\": [
+                        {
+                            \"client_id\": \"ethan\",
+                            \"hostname\": \"ethan.local\",
+                            \"version\": \"V2\",
+                            \"remote_address\": \"1.2.3.4:33576\",
+                            \"state\": 3,
+                            \"ready_count\": 1,
+                            \"in_flight_count\": 1,
+                            \"message_count\": 1396,
+                            \"finish_count\": 1395,
+                            \"requeue_count\": 0,
+                            \"connect_ts\": 1549065745,
+                            \"sample_rate\": 0,
+                            \"deflate\": false,
+                            \"snappy\": false,
+                            \"user_agent\": \"nsq_tail/1.1.0 go-nsq/1.0.6\",
+                            \"tls\": false,
+                            \"tls_cipher_suite\": \"\",
+                            \"tls_version\": \"\",
+                            \"tls_negotiated_protocol\": \"\",
+                            \"tls_negotiated_protocol_is_mutual\": false
+                        }
+                    ],
+                    \"paused\": false,
+                    \"e2e_processing_latency\": {
+                        \"count\": 0,
+                        \"percentiles\": null
+                    }
+                }
+            ],
+            \"depth\": 0,
+            \"backend_depth\": 0,
+            \"message_count\": 29259,
+            \"paused\": false,
+            \"e2e_processing_latency\": {
+                \"count\": 0,
+                \"percentiles\": null
+            }
+        }
+    ],
+    \"memory\": {
+        \"heap_objects\": 21625,
+        \"heap_idle_bytes\": 11886592,
+        \"heap_in_use_bytes\": 3743744,
+        \"heap_released_bytes\": 10280960,
+        \"gc_pause_usec_100\": 5612,
+        \"gc_pause_usec_99\": 3742,
+        \"gc_pause_usec_95\": 878,
+        \"next_gc_bytes\": 4194304,
+        \"gc_total_runs\": 219
+        }
+    }
+}";
+
+    let body: StatusTopicsResponse = serde_json::from_str(body).unwrap();
+
+    let (queued, in_flight) = extract_size_from_body(body.data, "foo").unwrap();
     assert_eq!(3, queued);
     assert_eq!(1, in_flight);
 }
